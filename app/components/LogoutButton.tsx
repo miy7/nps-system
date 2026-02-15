f5573863fd0525b221a7f2a4b512d7fd56baadbb
devyim <txhshg@gmail.com>
2026-02-15 14:30:23 +0700

    Add LogoutButton component

---
+++ app/components/LogoutButton.tsx
@@ -0,0 +1,13 @@
+"use client";
+import { signOut } from "next-auth/react";
+
+export default function LogoutButton() {
+  return (
+    <button
+      onClick={() => signOut()}
+      className="bg-red-500 text-white px-4 py-2 rounded-lg hover:bg-red-600 text-sm font-medium transition"
+    >
+      ออกจากระบบ
+    </button>
+  );
+}
